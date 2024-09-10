03d1d8b66f492bbceecba4c059aee0bb0186c58c
Matthew Carter <mbc6132@psu.edu>
2024-09-10 00:21:35 -0400

    FINISHED

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "MBC6132";
+    char STUDENT_ID[] = "928873330";
 
     return 0 ;
 }
+
+
